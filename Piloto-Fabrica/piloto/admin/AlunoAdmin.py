ffee753e00db3b16ad1b651b49b02822255dad28
Jonata <jonataR75@gmail.com>
2024-11-25 01:35:21 -0300

    up

---
+++ Piloto-Fabrica/piloto/admin/AlunoAdmin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from piloto.models import Aluno
+
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('nomeCompleto', 'cpf', 'matricula', 'curso','getCampus', 'formaIngresso', 'situacao', 'dataCadastro', 'ativo')
+    search_fields = ('nomeCompleto', 'cpf', 'matricula')
+    list_filter = ('curso', 'situacao', 'formaIngresso', 'ativo' )
+    readonly_fields = ()
+    exclude = ('matricula', 'dataCadastro', 'ativo',)
+
+    def getCampus(self, obj):
+        return obj.curso.campus.nome  
+    getCampus.short_description = 'Campus'
+
---
+++ Piloto-Fabrica/piloto/views/LoginView.py
@@ -0,0 +1,19 @@
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class LoginViewAPI(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            return Response({'access_token': access_token}, status=status.HTTP_200_OK)
+        else:
+            return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
